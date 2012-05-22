e7ec25cb32e3814ccb5a10099833fee26ca8d0a1
jebbailey <baileyji@umich.edu>
2012-05-22 10:41:14 -0400

    added cs() to connect to shoe

--- toy programs/interactiveAgentTest.py
+++ toy programs/interactiveAgentTest.py
@@ -1,4 +1,5 @@
 import socket
+SHOE_R=('localhost',42000)
 GALIL_R=('localhost',40000)
 DIRECTOR=('141.211.198.209',51280)#socket.gethostname(),50000)
 
@@ -7,6 +8,11 @@ def cg():
     s.connect(GALIL_R)
     return s
     
+def cs():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(SHOE_R)
+    return s
+
 def cd():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(DIRECTOR)

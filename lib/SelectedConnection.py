bb10a163decf32e5fca355186adbf3a7c5d5eec4
jebbailey <baileyji@umich.edu>
2012-05-22 10:44:42 -0400

    Changes port fetching functionality

--- lib/SelectedConnection.py
+++ lib/SelectedConnection.py
@@ -10,6 +10,7 @@ class ConnectError(IOError):
 class SelectedConnection():
     def __init__(self, logger=None,
                 default_message_recieved_callback=None,
+                default_message_received_callback=None,
                 default_message_sent_callback=None,
                 default_message_error_callabck=None):
 

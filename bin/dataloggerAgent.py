5478080bbdf892e7339d787454afc4c8af425a3d
jebbailey <baileyji@umich.edu>
2012-06-18 16:17:07 -0400

    Datalogging works (sans actual logging)!

--- bin/dataloggerAgent.py
+++ bin/dataloggerAgent.py
@@ -6,47 +6,42 @@ import signal
 import logging
 import logging.handlers
 import atexit
-import serial
 import sys
 import select
-sys.path.append('./Galil/')
-import galil
-from HandledSocket import HandledSocket
+sys.path.append('../lib/')
 from agent import Agent
 from command import Command
 import sqlite3
-
-MAX_CLIENTS=2
+import datalogger
+import m2fsConfig
 
 class DataloggerAgent(Agent):
     def __init__(self):
-        Agent.__init__(self,'Datalogger Agent')
+        Agent.__init__(self,'DataloggerAgent')
         
         #Initialize the dataloggers
-        self.dataloggerR=datalogger.Datalogger('/dev/dataloggerR', self.logger)
-        self.dataloggerB=datalogger.Datalogger('/dev/dataloggerB', self.logger)
-        self.dataloggerC=datalogger.Datalogger('/dev/dataloggerC', self.logger)
-        self.devices.append(dataloggerR)
-        self.devices.append(dataloggerB)
-        self.devices.append(dataloggerC)
-        
-
-        
+        self.dataloggerR=datalogger.Datalogger('/dev/dataloggerR', 115200, self.logger)
+        self.dataloggerB=datalogger.Datalogger('/dev/dataloggerB', 115200, self.logger)
+        self.dataloggerC=datalogger.Datalogger('/dev/dataloggerC', 115200, self.logger)
+        self.devices.append(self.dataloggerR)
+        self.devices.append(self.dataloggerB)
+        self.devices.append(self.dataloggerC)
+        self.currentTemps={}
+        self.command_handlers={
+                    'TEMPS':self.TEMPS_command_handler,
+                    'VERSION':self.version_request_command_handler,
+                    'STATUS':self.status_command_handler}
+        #self.temp_database_file=m2fsConfig.getTempDatafile()
         #open the logging database TODO
-        self.num_temps=5
-        self.database=sqlite3.connect(self.database_file)
-        self.initialize_database()
-        #open the connection to the fiber shoes
-        #shoeR=
-        #shoeB=
-
-    def initialize_database(self)
+        #self.database=sqlite3.connect(self.database_file)
+        #self.initialize_database()
+    
+    def initialize_database(self):
         dataloggerR_temp_column_names='HiRes R, LoRes R, Prism R'
         dataloggerC_temp_column_names='CaF R, Triplet R, Ambient R, Triplet B, CaF B, Ambient B'
         dataloggerB_temp_column_names='HiRes B, LoRes B, Prism B'
         shoeB_temp_column_name='Shoe B'
         shoeR_temp_column_name='Shoe R'
-        
         temps_table_definition=(
             "Temps(Time timestamp primary key, " +
                 (''.join(map(lambda x: "Temp %i real, " % x,
@@ -56,52 +51,30 @@ class DataloggerAgent(Agent):
             "Rx int, Ry int, Rz int, "+
             "Cx int, Cy int, Cz int, "+
             "Bx int, By int, Bz int")
-        self.database.execute("create table if not exists "+temps_table_definition)
-        self.database.execute("create table if not exists "+accel_table_definition)
-        
+        #self.database.execute("create table if not exists "+temps_table_definition)
+        #self.database.execute("create table if not exists "+accel_table_definition)
     
     def listenOn(self):
         return ('localhost', self.PORT)
-      
+    
+    def on_exit(self, arg):
+        """Prepare to exit"""
+        #TODO close and save data file
+        Agent.on_exit(self, arg)
+    
     def get_version_string(self):
         return 'Datalogger Agent Version 0.1'
     
-    def socket_message_recieved_callback(self, source, message_str):
-        """Create and execute a Command from the message"""
-        if False:#message_str is malformed:
-            command=Command(source, message_str, state='complete',
-                reply='!ERROR: Malformed Command', callback=None)
-        else:
-            command=Command(source, message_str, callback=None)
-            existing_commands_from_source=filter(lambda x: x.source==source, self.commands)
-            if not existing_commands_from_source:
-                command_handlers={
-                    'TEMPS':self.report_current_temps_command_handler,
-                    'STATUS':self.report_status_command_handler}
-                command_class=message_str.partition(' ')[0].partition('_')[0]
-                command_handlers.get(command_class.upper(), self.bad_command_handler)(command)
-            else:
-                #...ignore and log error
-                self.logger.warning(
-                    'Command %s recieved before command %s finished.' %
-                    (message_str, existing_commands_from_source[0].string))
-    
-    def report_current_temps_command_handler(self, command):
+    def TEMPS_command_handler(self, command):
         """ report the current temperatures """
-        temps=self.get_current_temps()
-        command.state='complete'
-        command.reply=''.join((len(temps)*"%f ")%temps)+'\n'
+        #gather current temps, expiring any older than XXX
+        command.setReply(''.join((len(temps)*"%f ")%temps))
     
-    def report_status_command_handler(self, command):
+    def status_command_handler(self, command):
         """ report status info """
-        #compile status info
-        #TODO
-        command.state='complete'
-        command.reply='TODO: current state of the dataloggers \n' #TODO
+        #TODO: compile status info
+        command.setReply('TODO: current state of the dataloggers.')
     
-    def get_current_temps(self):
-        return self.current_temps
-
     def insert_dataloggerR_temps_in_database(self, timestamp, temps):
         try:
             with self.database:
@@ -111,49 +84,77 @@ class DataloggerAgent(Agent):
                     thread(dataloggerR_temp_column_names, temps).append(timestamp.IN_MINUTES))
         except sqlite3.IntegrityError:
             pass
-
+    
     def run(self):
         """ execute once per loop, after select has run & before command closeout """
-        num_R_temps=3
-        num_C_temps=6
-        num_B_temps=3
-        if self.dataloggerR.have_unfetched_temps():
-            timestamp,temps=self.dataloggerR.fetch_temps()
-            #self.insert_dataloggerR_temps_in_database(timestamp,temps)
-            if timestamp[0]>self.most_current_dataloggerR_timestamp:
-                self.most_current_dataloggerR_timestamp=timestamp[0]
-                self.current_temps[0:num_R_temps]=temps
-        if self.dataloggerC.have_unfetched_temps():
-            timestamp,temps=self.dataloggerC.fetch_temps()
-            #self.insert_dataloggerC_temps_in_database(timestamp,temps)
-            if timestamp[0]>self.most_current_dataloggerC_timestamp:
-                self.most_current_dataloggerC_timestamp=timestamp[0]
-                self.current_temps[num_R_temps:
-                    num_R_temps+num_C_temps]=temps
-        if self.dataloggerB.have_unfetched_temps():
-            timestamp,temps=self.dataloggerB.fetch_temps()
-            #self.insert_dataloggerB_temps_in_database(timestamp,temps)
-            if timestamp[0]>self.most_current_dataloggerB_timestamp:
-                self.most_current_dataloggerB_timestamp=timestamp[0]
-                self.current_temps[num_R_temps+num_C_temps:
-                    num_R_temps+num_C_temps+num_B_temps]=temps
-
-        #once per minute start a query to the shoes for temps
-        #TODO
         
+        #Temp handling
+        if self.dataloggerR.have_unfetched_temps():
+            timestamps,temps=self.dataloggerR.fetch_temps()
+            if timestamps[0]>self.most_current_dataloggerR_timestamp:
+                self.most_current_dataloggerR_timestamp=timestamps[0]
+                self.currentTemps['R']=temps
+            self.insert_dataloggerR_temps_in_database(timestamps[0],temps)
+        if self.dataloggerC.have_unfetched_temps():
+            timestamps,temps=self.dataloggerC.fetch_temps()
+            if timestamps[0]>self.most_current_dataloggerC_timestamp:
+                self.most_current_dataloggerC_timestamp=timestamps[0]
+                self.currentTemps['C']=temps
+            self.logger.debug("TempsC: %s:%s" % 
+                (time.asctime(time.localtime(long(timestamps[0]))), temps))
+            #self.insert_dataloggerC_temps_in_database(timestamps[0],temps)
+        if self.dataloggerB.have_unfetched_temps():
+            timestamps,temps=self.dataloggerB.fetch_temps()
+            if timestamps[0]>self.most_current_dataloggerB_timestamp:
+                self.most_current_dataloggerB_timestamp=timestamps[0]
+                self.currentTemps['B']=temps
+            self.insert_dataloggerB_temps_in_database(timestamps[0],temps)
+        #once per minute start a query to the shoes for temps
+        if False:
+            self.shoeR.sendMessage('TEMP', 
+                responseCallback=self.recieveFromRShoe,
+                errorCallback=self.recieveFromRShoe)
+            self.shoeB.sendMessage('TEMP',
+                responseCallback=self.recieveFromBShoe,
+                errorCallback=self.recieveFromBShoe)
+            #reset time to query shoe
+        
+        #Accelerometer handling
+        if self.dataloggerC.have_unfetched_accels():
+            timestamps,accels=self.dataloggerC.fetch_accels()
+            self.logger.debug("AccelsC: %s:%s" % 
+                (time.asctime(time.localtime(long(timestamps[0]))), len(accels)))
         
         #check that the dataloggers are online
         #TODO
+        
+        
+        
 
+    def recieveFromBShoe(self, message):
+        try:
+            self.currentTemps['shoeB']=float(message)
+            self.most_current_shoeB_timestamp=time.time()
+        except ValueError:
+            pass
+
+    def recieveFromRShoe(self, message):
+        try:
+            self.currentTemps['shoeR']=float(message)
+            self.most_current_shoeR_timestamp=time.time()
+        except ValueError:
+            pass         
 
     def runSetup(self):
         """ execute before main loop """
         self.most_current_dataloggerR_timestamp=0
         self.most_current_dataloggerC_timestamp=0
         self.most_current_dataloggerB_timestamp=0
-
+        self.most_current_shoeR_timestamp=0
+        self.most_current_shoeB_timestamp=0
+    
 
 if __name__=='__main__':
-    agent=GalilAgent()
+    agent=DataloggerAgent()
     agent.main()
 
--- lib/datalogger.py
+++ lib/datalogger.py
@@ -1,88 +1,100 @@
-import serial, termios, time
+import serial, termios, time, re
 from construct import *
+import SelectedConnection
 
 class DataloggerStartupException(Exception):
     pass
 
-class Datalogger(SelectedSerial):
+class Datalogger(SelectedConnection.SelectedSerial):
     """ Datalogger  Controller Class """
-    def __init__(self, portName, logger):
+    def __init__(self, *args,**kwargs):
         """open a threaded serial port connection with the controller
         assert the controller is running the correct version of the code
         """
+        self.mode='default'
         self.n_temp_sensors=5
-        SelectedSerial.__init__(self, portName, 115200, logger,
-            default_message_recieved_callback=
-            default_message_sent_callback=
-            )
+        self.messageHandler=None
+        self._have_unfetched_temps=False
+        self._have_unfetched_accels=False
+        SelectedConnection.SelectedSerial.__init__(self, *args,**kwargs)
     
     def handle_read(self):
         """Read from serial. Call callback"""
         try:
-            self.in_buffer = self.in_buffer + self.serial.read(self.serial.inWaiting())
-        except serial.SerialException, err:
+            self.in_buffer += self.connection.read(self.connection.inWaiting())
+        except Exception, err:
             self.handle_error(str(err))
-        if in_buffer:
+        if self.in_buffer:
             if self.mode=='default':
                 byteIn=self.in_buffer[0]
-                in_bufer=in_buffer[1:]
+                self.in_buffer=self.in_buffer[1:]
+                if byteIn not in 't?BE#L':
+                    self.logger.debug("Out of sync")
+                    matchobject=re.search('[t?BE#L]', self.in_buffer)
+                    if matchobject:
+                        byteIn=self.in_buffer[matchobject.start(0)]
+                        self.in_buffer=self.in_buffer[matchobject.start(0)+1:]
+                    else:
+                        self.in_buffer=''
+                        byteIn=''
                 if  byteIn == 't':
-                    self.logger.debug("Telling time to %s"% self)
+                    #self.logger.debug("Telling time to %s"% self)
                     self.send_time_to_datalogger()
                 elif byteIn == '?':
-                    self.logger.debug("Ping...pong on %s"% self)
-                    self.serial.write('!')
+                    self.logger.debug("Ping...pong %s" % self.addr_str())
+                    self.connection.write('!')
                 elif byteIn == 'B':
                     self.logger.debug("Battery status incomming on %s"% self)
                     self.length_of_incomming_message=X #TODO
                     self.mode=='listenN'
-                    self.responseCallback=receiveBatteryStatus
+                    self.messageHandler=self.receiveBatteryStatus
                 elif byteIn == 'E':
-                    self.logger.debug("Error message incomming from %s"% self)
+                    #self.logger.debug("Error message incomming from %s"% self)
                     self.mode='listen/n'
-                    self.responseCallback=receiveError
+                    self.messageHandler=self.receiveError
                 elif byteIn == '#':
-                    self.logger.debug("Debug message incomming from %s"% self)
+                    #self.logger.debug("Debug message incomming from %s"% self)
                     self.mode='listen/n'
-                    self.responseCallback=recieveDebugMessage
+                    self.messageHandler=self.receiveDebugMessage
                 elif byteIn == 'L':
-                    self.logger.debug("Logger data incomming from %s"% self)
-                    mode='listen4N'
-                    self.responseCallback=receiveLogData, which must but a '#' into out_buffer once the message is recieved
-                else:
-                    #likely we somehow got our of sync and should recover TODO
-                    pass
-            if mode=='listen4N' and len(in_bufer)>0: # listen for number of bytes to listen for mode
-                self.length_of_incomming_message=in_buffer[0]
-                in_bufer=in_buffer[1:]
-                mode='listenN'
-            if mode=='listenN' and len(in_buffer) >= self.length_of_incomming_message: #listen for N bytes mode
+                    #self.logger.debug("Logger data incomming from %s"% self)
+                    self.mode='listen4N'
+                    #callback must put '#' into out_buffer once message is received
+                    self.messageHandler=self.receiveLogData
+            if self.mode=='listen4N' and len(self.in_buffer)>0: 
+                # listen for number of bytes to listen for mode
+                self.length_of_incomming_message=ord(self.in_buffer[0])
+                self.in_buffer=self.in_buffer[1:]
+                #self.logger.debug("listen4N message of length %i" % 
+                #    self.length_of_incomming_message)
+                self.mode='listenN'
+            if self.mode=='listenN' and len(self.in_buffer) >= self.length_of_incomming_message: 
+                #listen for N bytes mode
                 message_str=self.in_buffer[:self.length_of_incomming_message]
                 self.in_buffer=self.in_buffer[self.length_of_incomming_message:]
-                self.logger.debug("Recieved message of length %i on %s" % 
-                    (self.length_of_incomming_message, self))
-                if self.responseCallback:
-                    callback=self.responseCallback
-                    self.responseCallback=self.defaultResponseCallback
-                    callback(self, message_str)
-                mode='default'
-            if mode=='listen/n':
+                #self.logger.debug("Received message of length %i on %s" % 
+                #    (self.length_of_incomming_message, self))
+                if self.messageHandler:
+                    callback=self.messageHandler
+                    self.messageHandler=None
+                    callback(message_str)
+                self.mode='default'
+            if self.mode=='listen/n':
                 count=self.in_buffer.find('\n')
                 if count is not -1:
                     message_str=self.in_buffer[0:count+1]
                     self.in_buffer=self.in_buffer[count+1:]
-                    self.logger.debug("Recieved message '%s' on %s" % 
-                        (message_str.replace('\n','\\n'), self))
-                    if self.responseCallback:
-                        callback=self.responseCallback
-                        self.responseCallback=self.defaultResponseCallback
-                        callback(self, message_str[:-1])
-                    mode='default'
+                    #self.logger.debug("Received message '%s' on %s" % 
+                    #    (message_str.replace('\n','\\n').replace('\r','\\r'), self))
+                    if self.messageHandler:
+                        callback=self.messageHandler
+                        self.messageHandler=None
+                        callback(message_str[:-1])
+                    self.mode='default'
         else:
-            # empty read indicates disconnection
-            self.logger.error("Empty read, should this happen on serial?.")
-            self.handle_disconnect()
-    
+            # empty read indicates disconnection on socket, should it even happen on serial
+            #self.logger.debug("Empty read, should this happen on serial?.")
+            pass
     
     def send_time_to_datalogger(self):
         """ send the current time to the datalogger"""
@@ -90,63 +102,66 @@ class Datalogger(SelectedSerial):
         hexutime=hex(utime)[2:].upper()
         s='t'+UBInt32("f").build(utime)
         self.logger.debug('Sending time as %s' % s)
-        #this is what it took to send the time in testing, oh if only self.serial.write(s) would work
-        self.serial.write(s[0])
-        self.serial.write('\x00'+s[1])
-        self.serial.write('\x00'+s[2])
-        self.serial.write('\x00'+s[3])
-        self.serial.write('\x00'+s[4])
+        #this is what it took to send the time in testing, oh if only self.connection.write(s) would work
+        self.connection.write(s[0])
+        self.connection.write('\x00'+s[1])
+        self.connection.write('\x00'+s[2])
+        self.connection.write('\x00'+s[3])
+        self.connection.write('\x00'+s[4])
         
     def receiveLogData(self, data):
         """ Convert logger data into a nice neat form and sit on it"""
-		Acceleration_Record_Length=8+6*32
-		Temp_Record_Length=8+4*self.n_temp_sensors
-		Combined_Record_Length=Acceleration_Record_Length+4*Num_Temp_Sensors
-		tempConstruct=StrictRepeater(Num_Temp_Sensors,LFloat32("temps"))
-		accelConstruct=StrictRepeater(FIFO_Length*3,SLInt16("accel"))
-		if len(data)==Combined_Record_Length:
+        Acceleration_Record_Length=8+6*32
+        Temp_Record_Length=8+4*self.n_temp_sensors
+        Combined_Record_Length=Acceleration_Record_Length+4*self.n_temp_sensors
+        tempConstruct=StrictRepeater(self.n_temp_sensors,LFloat32("temps"))
+        accelConstruct=StrictRepeater(32*3,SLInt16("accel"))
+        if len(data)==Combined_Record_Length:
             self._have_unfetched_accels=True
             self._have_unfetched_temps=True
-			self.current_temps=tempConstruct.parse(record[0:4*Num_Temp_Sensors+1])
-			self.current_accels=accelConstruct.parse(record[4*Num_Temp_Sensors+1:-8])
+            self.current_temps=tempConstruct.parse(data[0:4*self.n_temp_sensors+1])
+            self.current_accels=accelConstruct.parse(data[4*self.n_temp_sensors+1:-8])
             self.most_recent_record_timestamp=(
-                ULInt32("foo").parse(record[-8:-4]),
-                ULInt32("foo").parse(record[-4:])
+                ULInt32("foo").parse(data[-8:-4]),
+                ULInt32("foo").parse(data[-4:])
                 )
             self.accels_timestamp=self.most_recent_record_timestamp
             self.temps_timestamp=self.most_recent_record_timestamp  
-		elif len(data)==Acceleration_Record_Length:
+        elif len(data)==Acceleration_Record_Length:
             self._have_unfetched_accels=True
-			self.current_accels=accelConstruct.parse(record[0:-8])
+            self.current_accels=accelConstruct.parse(data[0:-8])
             self.most_recent_record_timestamp=(
-                ULInt32("foo").parse(record[-8:-4]),
-                ULInt32("foo").parse(record[-4:])
+                ULInt32("foo").parse(data[-8:-4]),
+                ULInt32("foo").parse(data[-4:])
                 )
             self.accels_timestamp=self.most_recent_record_timestamp
-		elif len(data)==Temp_Record_Length:
+        elif len(data)==Temp_Record_Length:
             self._have_unfetched_temps=True
-			self.current_temps=tempConstruct.parse(record[0:-8])
+            self.current_temps=tempConstruct.parse(data[0:-8])
             self.most_recent_record_timestamp=(
-                ULInt32("foo").parse(record[-8:-4]),
-                ULInt32("foo").parse(record[-4:])
+                ULInt32("foo").parse(data[-8:-4]),
+                ULInt32("foo").parse(data[-4:])
                 )
             self.temps_timestamp=self.most_recent_record_timestamp
         #NB: to convert accels to Gs and numpy array do:
         # 0.00390625*numpy.array(accels).reshape([32,3])
-        #time.strftime("%a, %d %b %Y %H:%M:%S +0000",time.localtime(record.unixtime))
-        self.serial.write('#')
+        #time.strftime("%a, %d %b %Y %H:%M:%S +0000",time.localtime(data.unixtime))
+        self.connection.write('#')
         
     def receiveDebugMessage(self, message):
         """ Process a debugging message from the datalogger"""
-        self.logger.debug("Datalogger %s: %s " % (self,message) )
+        cleanmsg=message.replace('\n','\\n').replace('\r','\\r')
+        self.logger.debug("%s: %s" % (self.addr_str(),cleanmsg) )
         
     def receiveError(self, message):
         """ Process an error message from the datalogger"""
-        self.logger.error("Datalogger %s: %s " % (self,message) )
+        cleanmsg=message.replace('\n','\\n').replace('\r','\\r')
+        self.logger.error("%s: %s" % (self.addr_str(),cleanmsg) )
 	
     def receiveBatteryStatus(self, message):
         """ Process an error message from the datalogger"""
-        self.logger.info("Datalogger %s battery at %s " % (self,message) )
+        cleanmsg=message.replace('\n','\\n').replace('\r','\\r')
+        self.logger.info("%s battery at %s" % (self.addr_str(),cleanmsg) )
     
     def have_unfetched_temps(self):
         return self._have_unfetched_temps
@@ -158,6 +173,6 @@ class Datalogger(SelectedSerial):
         self._have_unfetched_temps=False
         return (self.temps_timestamp, self.current_temps)
 
-    def fetch_accelss(self):
+    def fetch_accels(self):
         self._have_unfetched_accels=False
         return (self.accels_timestamp, self.current_accels)

b6f294d55ec25060858d8fff877d37b58a779820
Jeb Bailey <baileyji@umich.edu>
2012-06-20 17:42:06 -0400

    Real udev rules

--- bin/plugController.py
+++ bin/plugController.py
@@ -16,27 +16,11 @@ from command import Command
 
 class ShoeAgent(Agent):
     def __init__(self):
-        Agent.__init__(self,'Slit Controller')
+        Agent.__init__(self,'PlugController')
         #Connect to the shoes
         self.agent_ports=m2fsConfig.getAgentPorts()
-        self.shoeAgentR_Connection=SelectedSocket('localhost',
-            agent_ports['shoeR'], self.logger)
-        self.devices.append(self.shoeAgentR_Connection)
-        self.shoeAgentB_Connection=SelectedSocket('localhost',
-            agent_ports['shoeB'], self.logger)
-        self.devices.append(self.shoeAgentB_Connection)
-    
-    def listenOn(self):
-        return ('localhost', self.PORT)
-    
-    def get_version_string(self):
-        return 'Slit Controller Version 0.1'
-    
-    def socket_message_recieved_callback(self, source, message_str):
-        """Create and execute a Command from the message"""
-        """Dispatch message to from the appropriate handler"""
-        command_handlers={
-            'SLITS':
+        self.command_handlers={
+            'PLATELIST':PLATELIST_command_handler,
             'SLITS_CLOSEDLOOP':
             'SLITS_SLITPOS':
             'SLITS_NOMINALPOS':
@@ -47,45 +31,23 @@ class ShoeAgent(Agent):
             'SLITS_HARDSTOP':
             'SLITS_IMAGSET':
             'SLITS_PROJSET':
-            'SLITS_STATUS':self.status_command_handler,
-            'SLITS_VERSION':self.version_request_command_handler}
-        command_name=message_str.partition(' ')[0].partition('_')[0]
-        command=Command(source, message_str)
-        if filter(lambda x: x.source==source, self.commands):
-            self.logger.warning('Command %s recieved before command %s finished.' %
-                (message_str, existing_commands_from_source[0].string))
-        else:
-            self.commands.append(command)
-            command_handlers.get(command_class.upper(), self.bad_command_handler)(command)
+            'STATUS':self.status_command_handler,
+            'VERSION':self.version_request_command_handler}
+    
+    def listenOn(self):
+        return ('localhost', self.PORT)
+    
+    def get_version_string(self):
+        return 'Slit Controller Version 0.1'
+    
+
+                    
+                    
+    def PLATELIST_command_handler(self, command):
+        """ Command is to get the list of available plates and their setups"""
+        command.setReply(self.get_string_of_available_plates_and_setups())
         
-           command_name=message_str.partition(' ')
-        if command_name not in commands.keys():
-            command=Command(source, message_str, state='complete',
-                reply='!ERROR: Malformed Command', callback=None)
-        else:
-            command=Command(source, message_str, callback=None)
-            existing_commands_from_source=filter(lambda x: x.source==source, self.commands)
-            if existing_commands_from_source: #...ignore and log error
-                self.logger.warning(
-                    'Command %s recieved before command %s finished.' %
-                    (message_str, existing_commands_from_source[0].string))
-            else:
-                self.commands.append(command)
-                
-                def responseCallback(response_string):
-                    command.state='complete'
-                    command.reply=response_string+'\n'
-                def errorCallback(response_string):
-                    command.state='complete'
-                    command.reply='!ERROR:'+response_string+'\n'
-                    
-                    
-        def plateConfig_command_handler(self, command):
-        command_name,junk,args=command.string.partition(' ')
-        if command_name=='PLATELIST':
-            """ Command is to get the list of available plates and their setups"""
-            command.state='complete'
-            command.reply=self.get_string_of_available_plates_and_setups()+'\n'
+    def PLATE_command_handler(self, command)
         elif command_name=='PLATE':
             """ Command is to get or set the current plate setup"""
             if '?' in command.string:

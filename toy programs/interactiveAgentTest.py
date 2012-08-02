97e8e765a7e710a771ef1ecc598c1a9cd55e2d0f
Jeb Bailey <baileyji@umich.edu>
2012-08-02 19:49:28 -0400

    Improvements to tool

--- toy programs/interactiveAgentTest.py
+++ toy programs/interactiveAgentTest.py
@@ -1,5 +1,6 @@
 import socket
 SHOE_R=('localhost',42000)
+SLITCONTROLLER=('localhost',48000)
 GALIL_R=('localhost',40000)
 DIRECTOR=('141.211.198.113',51280)#socket.gethostname(),50000)
 
@@ -11,9 +12,21 @@ def cg():
 def cs():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(SHOE_R)
+    s.settimeout(1)
+    return s
+
+def csc():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(SLITCONTROLLER)
+    s.settimeout(1)
     return s
 
 def cd():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(DIRECTOR)
+    s.settimeout(1)
     return s
+
+def sendrec(socket,msg):
+    socket.send(msg+'\n')
+    return socket.recv(1024)

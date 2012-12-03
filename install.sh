ee4c2e78e29c031e74ae2212fc9948a11fe0a034
Jeb Bailey <baileyji@umich.edu>
2012-12-02 22:42:06 -0800

    Add m2fsuser and m2fsadmin to install script along with creating of plate directories

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cp -rv ./etc /etc
+useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' "m2fsuser") m2fsuser
+useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' "m2fsadmin") m2fsadmin
+mkdir -p /home/m2fs/plates
+mkdir -p /home/m2fsuser/copy_platefiles_into_me
+ln -s /home/m2fs/plates /home/m2fsuser/plates
 systemctl daemon-reload
 systemctl enable director.service
 systemctl condrestart director.service

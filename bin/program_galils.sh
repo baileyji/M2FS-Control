a416449767a63b439191cf347eff0773fe3566bc
Jeb Bailey <baileyji@umich.edu>
2012-12-07 14:23:08 -0800

    Create galil programming script

---
+++ bin/program_galils.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#  program_galils.sh
+#  m2fs
+#
+#  Created by Jeb Bailey on 12/7/12.
+#  Copyright (c) 2012 SpaceColonyOne, Inc. All rights reserved.
+systemctl stop galilR.service
+systemctl stop galilB.service
+./galilProgrammer.py -f ../Galil/m2fs.dmc -d /dev/galilR --auto
+./galilProgrammer.py -f ../Galil/m2fs.dmc -d /dev/galilB --auto
+systemctl start galilR.service
+systemctl start galilB.service

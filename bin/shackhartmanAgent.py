cc8b6d66c81308bc5c8fff93485de0c2bebdcb98
Jeb Bailey <baileyji@umich.edu>
2012-12-13 13:02:05 -0800

    shackhartmanAgent ready for testing

--- bin/shackhartmanAgent.py
+++ bin/shackhartmanAgent.py
@@ -24,7 +24,8 @@ class ShackHartmanAgent(Agent):
         self.max_clients=1
         self.command_handlers.update({
             'SHLED':self.SHLED_command_handler,
-            'SHLENS':self.SHLENS_command_handler})
+            'SHLENS':self.SHLENS_command_handler,
+            'TEMP':self.TEMP_command_handler})
     
     def listenOn(self):
         return ('localhost', self.PORT)
@@ -44,7 +45,7 @@ class ShackHartmanAgent(Agent):
                 self.shledValue=int(command_parts[1])
                 command.setReply('OK')
             except ValueError:
-                self.invalid_command_handler(command)
+                self.bad_command_handler(command)
             except IOError, e:
                 command.setReply('ERROR: %s' % str(e))
     
@@ -54,44 +55,86 @@ class ShackHartmanAgent(Agent):
             position=self.determineLensletPosition()
             command.setReply(position)
         else:
-            #TODO add in command response
-            if 'IN' in command.string and 'OUT' not in command.string:
-                self.shlenslet.sendMessageBlocking('\x89\x7F')
-            elif 'OUT' in command.string and 'IN' not in command.string:
-                self.shlenslet.sendMessageBlocking('\x8A\x7F')
+            err=self.getErrorStatus()
+            if err !='0x0':
+                command.setReply('ERROR: %s' % err)
             else:
-                command.setReply('!ERROR: Improperly formatted command.\n')
-
+                try:
+                    if 'IN' in command.string and 'OUT' not in command.string:
+                        self.shlenslet.sendMessageBlocking('\x89\x7F')
+                    elif 'OUT' in command.string and 'IN' not in command.string:
+                        self.shlenslet.sendMessageBlocking('\x8A\x7F')
+                    else:
+                        self.bad_command_handler(command)
+                except IOError, e:
+                    command.setReply('ERROR: %s' % e)
+    
+    def TEMP_command_handler(self, command):
+        """report temperature in deg C, temps below 0 = 0"""
+        command.setReply(self.getTemp())
+    
     def status_command_handler(self, command):
         """report status"""
-        try:
-            lensStatus=self.determineLensletPosition()
-        except IOError:
-            lensStatus='ERROR'
+        lensStatus=self.determineLensletPosition()
+        temp=self.getTemp()
+        err=self.getErrorStatus()
         try:
             self.shled.sendMessage(chr(self.shledValue))
             ledStatus='%i' % self.shledValue
         except IOError:
             ledStatus='ERROR'
-        command.setReply('Lenslet: %s Led: %s' % (lensStatus, ledStatus))
+        command.setReply('Lenslet:%s Led:%s Temp:%s Err:%s' %
+                         (lensStatus, ledStatus, temp, err))
+    
+    def getErrorStatus(self):
+        """
+        Poll the controller for the error status byte
+        
+        Returns the error value as a hex string or ERROR if IOERROR
+        """
+        try:
+            self.shlenslet.sendMessageBlocking('\xA1\x00')
+            response=self.shlenslet.receiveMessageBlocking(nBytes=2)
+            #for bit meanings se simple_motor_controllers.pdf
+            err=hex(256*ord(response[1])+ord(response[0]))
+        except IOError:
+            err='ERROR'
+        return err
+    
+    def getTemp(self):
+        """
+        Poll the controller for the temp
+        
+        Returns the temp value as a string or ERROR if IOERROR
+        """
+        try:
+            self.shlenslet.sendMessageBlocking('\xA1\x18')
+            response=self.shlenslet.receiveMessageBlocking(nBytes=2)
+            temp=str(0.1*(ord(response[0])+256*ord(response[1])))
+        except IOError:
+            temp='ERROR'
+        return temp
     
     def determineLensletPosition(self):
-        self.shlenslet.sendMessageBlocking('\xA1\x21')
-        response=self.shlenslet.receiveMessageBlocking()
-        if response != 0:
-            return 'MOVING'
-        else:
-            self.shlenslet.sendMessageBlocking('\xA1\x12')
+        try:
+            self.shlenslet.sendMessageBlocking('\xA1\x21')
             response=self.shlenslet.receiveMessageBlocking()
-            if response > 1024: #limit is NC with pullup on 12bit ADC 
-                return 'IN'
+            if response != 0:
+                return 'MOVING'
             else:
-                self.shlenslet.sendMessageBlocking('\xA1\x16')
+                self.shlenslet.sendMessageBlocking('\xA1\x12')
                 response=self.shlenslet.receiveMessageBlocking()
-                if response > 1024:
-                    return 'OUT'
+                if ord(response[0]) > 64: #limit is NC with pullup on 12bit ADC
+                    return 'IN'
                 else:
-                    return 'INTERMEDIATE'
+                    self.shlenslet.sendMessageBlocking('\xA1\x16')
+                    response=self.shlenslet.receiveMessageBlocking()
+                    if ord(response[0]) > 64:
+                        return 'OUT'
+                    else:
+                        return 'INTERMEDIATE'
+        except IOError:
+            return 'ERROR'
 
 if __name__=='__main__':
     agent=ShackHartmanAgent()

2f3b7d6deeb470f517401e24d2cf39bd82a9e077
Jeb Bailey <baileyji@umich.edu>
2013-02-23 12:00:30 -0800

    Finishing touches on guider_udev_namer.py

--- bin/guider_udev_namer.py
+++ bin/guider_udev_namer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python2.7
-import os
-f=open('/udevtest.log','a')
-#f.write(str(os.environ))
+import os, sys
 isGuider=os.environ.get('ID_USB_INTERFACE_NUM',None)=='00'
-#f.close()
 if isGuider:
-        exit(0)
+        sys.exit(0)
 else:
-        exit(1)
+        sys.exit(1)
 
 

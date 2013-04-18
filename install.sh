3a1a2e89303505e384cdb0aa71ee9f42b06d8195
Jeb Bailey <baileyji@umich.edu>
2013-04-18 11:08:57 -0400

    Update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+easy_install pip
+pip install ipython
+pip install construct
+opkg install ntp.systemd
+
 cp -rv ./etc/* /etc/
-useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' "m2fsuser") m2fsuser
-useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' "m2fsadmin") m2fsadmin
-mkdir -p /home/m2fs/plates
-mkdir -p /home/m2fsuser/copy_platefiles_into_me
-ln -s /home/m2fs/plates /home/m2fsuser/plates
+#useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' "m2fsuser") m2fsuser
+#useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' "m2fsadmin") m2fsadmin
 systemctl daemon-reload
 systemctl enable director.service
+systemctl enable ethernet_hack.service
+systemctl enable nut-server.service
+systemctl condrestart nut-server.service
 systemctl condrestart director.service

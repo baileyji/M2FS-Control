828394098c90b784566dd41b6f48a7d3a85fe854
Jeb Bailey <baileyji@umich.edu>
2013-04-25 12:46:55 -0700

    Reorder installer script

--- install.sh
+++ install.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
-easy_install pip
-pip install ipython
-pip install construct
-opkg install ntp.systemd
 
+#Install M2FS system config files
 cp -rv ./etc/* /etc/
 cp -v ./bashrc /home/root/.bashrc
 cp -v ./vimrc /home/root/.vimrc
+#Ensure systemd loads the new units
+systemctl daemon-reload
+
+#Bring ethernet online
+systemctl enable ethernet_hack.service
+systemctl start ethernet_hack.service
+
+#Bring UPS monitoring online
+systemctl enable nut-server.service
+systemctl start nut-server.service
+
+#Get ntp installed
+opkg install ntp.systemd
+
+#Get pip
+easy_install pip
+
+#install ipython and needed python packages
+pip install ipython
+pip install construct
 
 #useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' "m2fsuser") m2fsuser
 #useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' "m2fsadmin") m2fsadmin
-systemctl daemon-reload
+
+#Enable and start the director
 systemctl enable director.service
-systemctl enable ethernet_hack.service
-systemctl enable nut-server.service
-systemctl condrestart nut-server.service
-systemctl condrestart director.service
+systemctl start director.service

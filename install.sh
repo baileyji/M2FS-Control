051805c9254e91e566e253f5d1285af57e31dd2e
Jeb Bailey <baileyji@umich.edu>
2013-04-25 12:11:34 -0700

    add bashrc and vimrc and update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ pip install construct
 opkg install ntp.systemd
 
 cp -rv ./etc/* /etc/
+cp -v ./bashrc /home/root/.bashrc
+cp -v ./vimrc /home/root/.vimrc
+
 #useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' "m2fsuser") m2fsuser
 #useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' "m2fsadmin") m2fsadmin
 systemctl daemon-reload

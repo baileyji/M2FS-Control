dc796020f762ed7245259f0e745bd1a7f1692ebb
Jeb Bailey <baileyji@umich.edu>
2013-04-25 14:47:07 -0700

    Fix nut installation

--- install.sh
+++ install.sh
@@ -12,6 +12,9 @@ systemctl enable ethernet_hack.service
 systemctl start ethernet_hack.service
 
 #Bring UPS monitoring online
+mkdir /var/state/ups
+chmod 0770 /var/state/ups
+chown root:run /var/state/ups
 systemctl enable nut-server.service
 systemctl start nut-server.service
 

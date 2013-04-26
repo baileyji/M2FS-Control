6ef754a468c6c909197646720e248701149ad128
Jeb Bailey <baileyji@umich.edu>
2013-04-25 21:40:44 -0700

    Shutter work

--- Arduino/shutter/shutter.ino
+++ Arduino/shutter/shutter.ino
@@ -34,14 +34,12 @@
 #include "position.h"
 
 
-POSITION red_pos, blue_pos, 
-         last_commanded_red, last_commanded_blue;
+POSITION red_pos, blue_pos, commanded_red, commanded_blue;
 
 Servo redshutter, blueshutter;
 
-unsigned long redDebounceTime, blueDebounceTime, redMoveTime, blueMoveTime;
 
-
+bool redPinValue, bluePinValue, redPinValueLast, bluePinValueLast;
 
 
 void setup() {
@@ -58,81 +56,83 @@ void setup() {
     blueshutter.attach(BLUE_SERVO_PIN);
     
     close_both_shutters();
+    
+    bluePinValue=digitalRead(BLUE_IN_PIN);
+    redPinValue=digitalRead(RED_IN_PIN);
+    bluePinValueLast=bluePinValue;
+    redPinValueLast=redPinValue;
+    
+    Serial.begin(115200);
 }
 
 
 void loop() {
 
   bool doBlueMove=false, doRedMove=false;
-          
-  POSITION commanded_red, commanded_blue;
-  unsigned long bounce_time, sampleTime;
+  
+  //Sample the TTL lines for the shutters, debounceing the inputs
+  
+  redPinValue=digitalRead(RED_IN_PIN);
+  bluePinValue=digitalRead(BLUE_IN_PIN);
+  if (bluePinValue != bluePinValueLast ||
+      redPinValue != redPinValueLast) {
+      
+      delay(DEBOUNCE_DELAY);
+    
+      bluePinValueLast=bluePinValue;
+      redPinValueLast=redPinValue;
+      bluePinValue=digitalRead(BLUE_IN_PIN);
+      redPinValue=digitalRead(RED_IN_PIN);
+    
+      if (bluePinValueLast==bluePinValue )
+        commanded_blue = bluePinValue ? OPEN:CLOSED;
+      if (redPinValueLast==redPinValue )
+        commanded_red = redPinValue ? OPEN:CLOSED;
+      
+  }
+  delay(DEBOUNCE_DELAY);
 
-  //Sample the TTL lines for the shutters
-  //  IN_PIN high -> Open Shutter (if open, do nothing)
-  //  IN_PIN low -> Close Shutter (if closed, do nothing)
-  sampleTime=millis();
-  
-  commanded_blue = digitalRead(BLUE_IN_PIN) ? OPEN:CLOSED;
-  commanded_red  = digitalRead(RED_IN_PIN)  ? OPEN:CLOSED;
+  //Do we need to move
+  doBlueMove=(commanded_blue != blue_pos);
+  doRedMove=(commanded_red != red_pos);
 
-  //Debounce the inputs
-  // If the commanded position is different from the last commanded position,
-  //   then get the time to debounce the input (just in case).
-  //   If the commanded position has remained constant for at least the debounce delay,
-  ///then act on the commanded position
-  
-  bounce_time=sampleTime;
-  if (commanded_blue != last_commanded_blue) {
-    blueDebounceTime=sampleTime;
-    last_commanded_blue=commanded_blue;
-  }
-  
-  if (commanded_red != last_commanded_red) {
-    redDebounceTime=sampleTime;
-    last_commanded_red=commanded_red;
-  }
-  
-  if (commanded_blue != blue_pos) {
-      doBlueMove=(bounce_time - blueDebounceTime > DEBOUNCE_DELAY);
-  }
-  else {
-      doBlueMove=false;
-  }
-  
-  if (commanded_red != red_pos) {
-      doRedMove=(bounce_time - redDebounceTime > DEBOUNCE_DELAY);
-  }
-  else {
-      doRedMove=false;
-  }
-  
   //Move the shutters as requested
+    if (doRedMove) {
+        if (commanded_red==CLOSED) Serial.println("Close R");
+        else Serial.println("Open R");
+    }
+    if (doBlueMove) {
+        if (commanded_blue==CLOSED) Serial.println("Close B");
+        else Serial.println("Open B");
+    }
   
   if (doBlueMove && commanded_blue==OPEN) open_blue_shutter();
-  if (doRedMove && commanded_red==OPEN) open_red_shutter();
-  if (doRedMove && doBlueMove &&
-      commanded_red==CLOSED && commanded_blue==CLOSED) {
+  if (doRedMove  && commanded_red==OPEN) open_red_shutter();
+  if (doRedMove  && commanded_red==CLOSED &&
+      doBlueMove && commanded_blue==CLOSED) {
       close_both_shutters();
   }
   else {
     if (doBlueMove && commanded_blue==CLOSED) close_blue_shutter();
     if (doRedMove && commanded_red==CLOSED) close_red_shutter();
   }
-  
+
 }
 
 void open_red_shutter() {
+Serial.println("opening R");
     redshutter.writeMicroseconds(RED_OPEN_US);
     red_pos=OPEN;
 }
 
 void open_blue_shutter() {
+Serial.println("opening B");
     blueshutter.writeMicroseconds(BLUE_OPEN_US);
     blue_pos=OPEN;
 }
 
 void close_red_shutter() {
+Serial.println("closing R");
     redshutter.writeMicroseconds(RED_PARTIAL_US);
     delay(CLOSE_TIME_MS);
     redshutter.writeMicroseconds(RED_CLOSED_US);
@@ -140,6 +140,7 @@ void close_red_shutter() {
 }
 
 void close_blue_shutter() {
+Serial.println("closing B");
     blueshutter.writeMicroseconds(BLUE_PARTIAL_US);
     delay(CLOSE_TIME_MS);
     blueshutter.writeMicroseconds(BLUE_CLOSED_US);
@@ -147,6 +148,7 @@ void close_blue_shutter() {
 }
 
 void close_both_shutters() {
+Serial.println("closing both");
     redshutter.writeMicroseconds(RED_PARTIAL_US);
     blueshutter.writeMicroseconds(BLUE_PARTIAL_US);
     delay(CLOSE_TIME_MS);

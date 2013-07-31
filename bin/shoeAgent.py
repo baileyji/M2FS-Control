8b92853a5df151511d48413ecfff6aa90a47103a
Jeb Bailey <baileyji@umich.edu>
2013-07-31 10:37:34 -0400

    Remove cruft

--- bin/shoeAgent.py
+++ bin/shoeAgent.py
@@ -323,11 +323,8 @@ class ShoeAgent(Agent):
         representing the closed position.
         """
         if '?' in command.string:
-            if self.command_worker_thread_running('SLITS'):
-                command.setReply(self.get_command_state('SLITS'))
-            else:
-                #Command the shoe to report the active slit for all 8 tetri
-                command.setReply(self._do_online_only_command('SG*'))
+            #Command the shoe to report the active slit for all 8 tetri
+            command.setReply(self._do_online_only_command('SG*'))
         else:
             #Vet the command
             command_parts=command.string.replace(',',' ').split(' ')

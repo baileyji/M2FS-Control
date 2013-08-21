10d294b91ec4a08b84637d12fd696b0e964f1d5a
Jeb Bailey <baileyji@umich.edu>
2013-08-21 10:10:07 -0400

    Work on ethernet adapter script for multiple interfaces.

--- bin/keep_ethernet_up_hack.py
+++ bin/keep_ethernet_up_hack.py
@@ -1,37 +1,60 @@
 #!/usr/bin/env python2.7
 import sys, dbus, time
 
-service_path = "/net/connman/service/ethernet_b88d1255cd7e_cable"
+public_adapter = "/net/connman/service/ethernet_b88d1255cd7e_cable"
+
+fls_adapter = "/net/connman/service/ethernet_b88d1255d1ee_cable"
+
+FLS_ADAPTER_ADDRESS=make_variant('192.168.0.1')
+FLS_ADAPTER_NETMASK=make_variant('255.255.255.0')
+
+CONNECT_TIMOUT=10000
 
 bus = dbus.SystemBus()
 
 def make_variant(string):
 	return dbus.String(string, variant_level=1)
 
-def getOnline():
-    service = dbus.Interface(bus.get_object('net.connman', service_path),
-                             'net.connman.Service')
+def bringPublicUp():
+    adapter = bus.get_object('net.connman', public_adapter)
+    service = dbus.Interface(adapter, 'net.connman.Service')
     service.SetProperty("IPv4.Configuration", {"Method": make_variant('dhcp') })
+    service.Connect(timeout=CONNECT_TIMOUT)
 
-def needToGetOnline():
+def bringFLSUp():
+    adapter=bus.get_object('net.connman', fls_adapter)
+    service = dbus.Interface(adapter, 'net.connman.Service')
+    setting_dict={"Method": make_variant('manual'),
+                  "Address":make_variant(FLS_ADAPTER_ADDRESS),
+                  "Netmask":make_variant(FLS_ADAPTER_NETMASK),
+                  "Gateway":make_variant(FLS_ADAPTER_ADDRESS)}
+    service.SetProperty("IPv4.Configuration", setting_dict)
+    service.Connect(timeout=CONNECT_TIMOUT)
+
+def adapterOffline(adapter_path):
     manager = dbus.Interface(bus.get_object('net.connman', '/'),
                              'net.connman.Manager')
     services = manager.GetServices()
     for entry in services:
         path = entry[0]
-        if path == service_path:
+        if path == adapter_path:
             properties = entry[1]
             #Check to see if state is listed as ready,
             # if so the we are not online and getOnline() should work
-            if properties['State'].lower().strip()=='ready':
+            if properties['State'].lower().strip()==['ready', 'idle']:
                 return True
     return False
 
 if __name__=='__main__':
     while True:
         try:
-            if needToGetOnline():
-                getOnline()
+            if adapterOffline(public_adapter):
+                bringPublicUp()
+        except Exception as e:
+            print str(e)
+        try:
+            if adapterOffline(fls_adapter):
+                bringFLSUp()
         except Exception as e:
             print str(e)
         time.sleep(30)

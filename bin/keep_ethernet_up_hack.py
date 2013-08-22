dbf6cabf66476de02f0a05dc17d47f0788a830be
Jeb Bailey <baileyji@umich.edu>
2013-08-22 13:58:03 -0400

    Work on ethernet adapter config.

--- bin/keep_ethernet_up_hack.py
+++ bin/keep_ethernet_up_hack.py
@@ -8,6 +8,9 @@ fls_adapter = "/net/connman/service/ethernet_b88d1255d1ee_cable"
 FLS_ADAPTER_ADDRESS=make_variant('192.168.0.1')
 FLS_ADAPTER_NETMASK=make_variant('255.255.255.0')
 
+PUBLIC_ADAPTER_ADDRESS=make_variant('200.28.147.41')
+PUBLIC_ADAPTER_NETMASK=make_variant('255.255.255.0')
+
 CONNECT_TIMOUT=10000
 
 bus = dbus.SystemBus()
@@ -15,19 +18,29 @@ bus = dbus.SystemBus()
 def make_variant(string):
 	return dbus.String(string, variant_level=1)
 
-def bringPublicUp():
+def bringPublicUpDHCP():
     adapter = bus.get_object('net.connman', public_adapter)
     service = dbus.Interface(adapter, 'net.connman.Service')
     service.SetProperty("IPv4.Configuration", {"Method": make_variant('dhcp') })
+    service.Connect(timeout=CONNECT_TIMOUT)
+
+def bringPublicUpFIXED():
+    adapter = bus.get_object('net.connman', public_adapter)
+    service = dbus.Interface(adapter, 'net.connman.Service')
+    setting_dict={"Method": make_variant('manual'),
+                  "Address":PUBLIC_ADAPTER_ADDRESS,
+                  "Netmask":PUBLIC_ADAPTER_NETMASK,
+                  "Gateway":PUBLIC_ADAPTER_ADDRESS}
+    service.SetProperty("IPv4.Configuration", setting_dict)
     service.Connect(timeout=CONNECT_TIMOUT)
 
 def bringFLSUp():
     adapter=bus.get_object('net.connman', fls_adapter)
     service = dbus.Interface(adapter, 'net.connman.Service')
     setting_dict={"Method": make_variant('manual'),
-                  "Address":make_variant(FLS_ADAPTER_ADDRESS),
-                  "Netmask":make_variant(FLS_ADAPTER_NETMASK),
-                  "Gateway":make_variant(FLS_ADAPTER_ADDRESS)}
+                  "Address":FLS_ADAPTER_ADDRESS,
+                  "Netmask":FLS_ADAPTER_NETMASK,
+                  "Gateway":FLS_ADAPTER_ADDRESS}
     service.SetProperty("IPv4.Configuration", setting_dict)
     service.Connect(timeout=CONNECT_TIMOUT)
 

2badfe0cab17097b6283b04f597db5ba26a792fa
Jeb Bailey <baileyji@umich.edu>
2019-09-15 14:44:24 -0700

    add an iPython cli script for testing

---
+++ bin/cli.py
@@ -0,0 +1,18 @@
+import socket
+from m2fsConfig import m2fsConfig
+
+def conn(thing, timeout=None):
+    if isinstance(thing, str):
+        thing = ('localhost' if thing.lower() != 'director' else socket.gethostname(),
+                 m2fsConfig.getPort(thing))
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(thing)
+    if timeout is not None:
+        s.settimeout(timeout)
+    return s
+
+
+def sr(socket, msg):
+    socket.send(msg + '\n')
+    return socket.recv(1024)

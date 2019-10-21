429ff95545dc259ecdfb73534b0b950134406521
Jeb Bailey <baileyji@umich.edu>
2019-10-20 17:39:31 -0700

    some oriental motor work

--- lib/orientalazd.py
+++ lib/orientalazd.py
@@ -5,18 +5,18 @@ from pymodbus.transaction import ModbusRtuFramer
 import numpy as np
 import logging, time, threading
 
-#TODO (general)
+# TODO (general)
 # -Modbus commands can be silently ignored and also not trip an alarm, e.g. move_to raises no errors
 # -ensure all communication commands only raise subclasses of IOError then remove the broad catches (or add
 #  specific ones if must to selectorAgent)
-#move_to needs to be robust
+# move_to needs to be robust
 
-#count= the number of registers to read
-#unit= the slave unit this request is targeting
-#address= the starting address to read from
+# count= the number of registers to read
+# unit= the slave unit this request is targeting
+# address= the starting address to read from
 
-CLIENTID=1
-PORT = '/dev/tty.usbserial-FT2KYRWY' #'/dev/thkrail'
+CLIENTID = 1
+PORT = '/dev/tty.usbserial-FT2KYRWY'  # '/dev/thkrail'
 BAUD = 230400
 
 ADDR_REMOTEIO = (0x007f, 2)
@@ -26,9 +26,9 @@ ADDR_OUTPUTS = (0x0178, 8)  # 0x0178-0x017F
 MAX_PULSE_RATE = 74000  # from docs should be 83333, however anything above ~74916 seems to trigger overspeed
 
 DEFAULT_SPEED_MMPERS = 28  # mm/s
-MM_TO_PULSE = 1/.0006
-DEFAULT_SPEED = int(round(MM_TO_PULSE*DEFAULT_SPEED_MMPERS))
-DEFAULT_ACCEL = int(round(MM_TO_PULSE*600))  # 0.6 m/s^2 is what the system came programmed with
+MM_TO_PULSE = 1 / .0006
+DEFAULT_SPEED = int(round(MM_TO_PULSE * DEFAULT_SPEED_MMPERS))
+DEFAULT_ACCEL = int(round(MM_TO_PULSE * 600))  # 0.6 m/s^2 is what the system came programmed with
 DEFAULT_DECEL = DEFAULT_ACCEL
 
 """
@@ -42,7 +42,7 @@ pymodbus.register_read_message.ReadWriteMultipleRegistersRequest 23
 """
 
 # Note null strings indicate bits reserved by OM, '-' indicates an unassigned/unused/NON-SIG bit
-DIRECT_IO_IN_BITS = ('FW-LS', '-', 'RV-LS', 'STOP-COFF', 'HOMES', '-', '-', 'ALM-RST', '-', '-',  'P-RESET', '',
+DIRECT_IO_IN_BITS = ('FW-LS', '-', 'RV-LS', 'STOP-COFF', 'HOMES', '-', '-', 'ALM-RST', '-', '-', 'P-RESET', '',
                      '-', '-', '-', '-')
 DIRECT_IO_OUT_BITS = ('HOME-END', 'IN-POS', 'PLS-RDY', 'READY', 'MOVE', 'ALM-B', '', '', '', '', '', '', '', '',
                       'ASG', 'BSG')
@@ -50,22 +50,27 @@ DIRECT_IO_OUT_BITS = ('HOME-END', 'IN-POS', 'PLS-RDY', 'READY', 'MOVE', 'ALM-B',
 REMOTE_IO_OUT_BITS = ('MBC', 'STOP-COFF_R', 'RV_LS_R', '-', 'FW-LS_R', 'READY', 'INFO', 'ALM-A', 'SYS-BSY',
                       'AREA0', 'AREA1', 'AREA2', 'TIM', 'MOVE', 'IN-POS', 'TLC')
 
-OUTPUT_BITS = ("HOME-END","ABSPEN","ELPRST-MON","-","-","PRST-DIS","PRST-STLD","ORGN-STLD","RND-OVF","FW-SLS","RV-SLS",
-               "ZSG", "RND-ZERO","TIM","-","MAREA","CONST-OFF","ALM-A","ALM-B","SYS-RDY","READY","PLS-RDY","MOVE",
-               "INFO","SYS-BSY","ETO-MON","IN-POS","-","TLC","VA","CRNT","AUTO-CD","MON-OUT","PLS-OUTR","-","-",
-               "USR-OUT0","USR-OUT1","-","-","-","-","-","-","-","-","-","-","AREA0","AREA1","AREA2","AREA3",
-               "AREA4","AREA5","AREA6","AREA7","MPS","MBC","RG","-","EDM","HWTOIN-MON","-","-","M-ACT0","M-ACT1",
-               "M-ACT2","M-ACT3","M-ACT4","M-ACT5","M-ACT6","M-ACT7","D-END0","D-END1","D-END2","D-END3","D-END4",
-               "D-END5","D-END6","D-END7","CRNT-LMTD","SPD-LMTD","-","-","OPE-BSY","PAUSE-BSY","SEQ-BSY",
-               "DELAY-BSY","JUMP0-LAT","JUMP1-LAT","NEXT-LAT","PLS-LOST","DCMD-RDY","DCMD-FULL","-","M-CHG",
-               "INFO-FW-OT","INFO-RV-OT","INFO-CULD0","INFO-CULD1","INFO-TRIP","INFO-ODO","-","-","-","-","-","-",
-               "INFO-DSLMTD","INFO-IOTEST","INFO-CFG","INFO-RBT","INFO-USRIO","INFO-POSERR","INFO-DRVTMP",
-               "INFO-MTRTMP","INFO-OVOLT","INFO-UVOLT","INFO-OLTIME","-","INFO-SPD","INFO-START","INFO-ZHOME",
-               "INFO-PR-REQ","-","INFO-EGR-E","INFO-RND-E","INFO-NET-E")
+OUTPUT_BITS = ("HOME-END", "ABSPEN", "ELPRST-MON", "-", "-", "PRST-DIS", "PRST-STLD", "ORGN-STLD", "RND-OVF", "FW-SLS",
+               "RV-SLS", "ZSG", "RND-ZERO", "TIM", "-", "MAREA", "CONST-OFF", "ALM-A", "ALM-B", "SYS-RDY", "READY",
+               "PLS-RDY", "MOVE", "INFO", "SYS-BSY", "ETO-MON", "IN-POS", "-", "TLC", "VA", "CRNT", "AUTO-CD",
+               "MON-OUT", "PLS-OUTR", "-", "-", "USR-OUT0", "USR-OUT1", "-", "-", "-", "-", "-", "-", "-", "-", "-",
+               "-", "AREA0", "AREA1", "AREA2", "AREA3", "AREA4", "AREA5", "AREA6", "AREA7", "MPS", "MBC", "RG", "-",
+               "EDM", "HWTOIN-MON", "-", "-", "M-ACT0", "M-ACT1",  "M-ACT2", "M-ACT3", "M-ACT4", "M-ACT5", "M-ACT6",
+               "M-ACT7", "D-END0", "D-END1", "D-END2", "D-END3", "D-END4",  "D-END5", "D-END6", "D-END7", "CRNT-LMTD",
+               "SPD-LMTD", "-", "-", "OPE-BSY", "PAUSE-BSY", "SEQ-BSY",  "DELAY-BSY", "JUMP0-LAT", "JUMP1-LAT",
+               "NEXT-LAT", "PLS-LOST", "DCMD-RDY", "DCMD-FULL", "-", "M-CHG",  "INFO-FW-OT", "INFO-RV-OT", "INFO-CULD0",
+               "INFO-CULD1", "INFO-TRIP", "INFO-ODO", "-", "-", "-", "-", "-", "-",  "INFO-DSLMTD", "INFO-IOTEST",
+               "INFO-CFG", "INFO-RBT", "INFO-USRIO", "INFO-POSERR", "INFO-DRVTMP",  "INFO-MTRTMP", "INFO-OVOLT",
+               "INFO-UVOLT", "INFO-OLTIME", "-", "INFO-SPD", "INFO-START", "INFO-ZHOME",  "INFO-PR-REQ", "-",
+               "INFO-EGR-E", "INFO-RND-E", "INFO-NET-E")
+
+
+def is_bit_set(value, bits, name):
+    return bool(value & (1 << bits.index(name)))
 
 
 def split_dword(x):
-    return x>>16, x&0xffff
+    return x >> 16, x & 0xffff
 
 
 def twos_complement(val, nbits):
@@ -80,12 +85,13 @@ def twos_complement(val, nbits):
     return val
 
 
-def merge_reg(reg, bytewid=1, rev=False):
+def merge_reg(reg, bytewid=8,
+              rev=False):  # Seems like rev should default to true since OM has high byters at lower register addr
     o = 0
     if rev:
         reg = reg[::-1]
     for i, r in enumerate(reg):
-        o |= r << (8*i*bytewid)
+        o |= r << (i * bytewid)
     return o
 
 
@@ -103,7 +109,7 @@ class OrientalMotor(object):
 
     def close(self):
         """Shortcut to disconnect(stop_and_break=True)"""
-        self.disconnet(True)
+        self.disconnect(True)
 
     def connect(self):
         self.modbus = ModbusClient(method="rtu", port=self.port, stopbits=1, bytesize=8, parity='E', baudrate=BAUD)
@@ -117,30 +123,30 @@ class OrientalMotor(object):
         """
         x = merge_reg(self.modbus.read_holding_registers(*ADDR_REMOTEIO, unit=1).registers)
         if pretty:
-            return tuple([v for i, v in enumerate(REMOTE_IO_OUT_BITS) if x & (1<<i) and v])
+            return tuple([v for i, v in enumerate(REMOTE_IO_OUT_BITS) if x & (1 << i) and v])
         else:
             return x
 
     def get_directOut(self, pretty=True):
         x = merge_reg(self.modbus.read_holding_registers(*ADDR_DIO, unit=1).registers)
         if pretty:
-            return tuple([v for i, v in enumerate(DIRECT_IO_OUT_BITS) if x & (1<<i) and v])
+            return tuple([v for i, v in enumerate(DIRECT_IO_OUT_BITS) if x & (1 << i) and v])
         else:
             return x
 
     def get_out(self, pretty=True):
         x = merge_reg(self.modbus.read_holding_registers(*ADDR_OUTPUTS, unit=1).registers, bytewid=2)
         if pretty:
-            return tuple([v for i, v in enumerate(OUTPUT_BITS) if x & (1<<i) and v])
+            return tuple([v for i, v in enumerate(OUTPUT_BITS) if x & (1 << i) and v])
         else:
             return x
 
     def get_temps(self):
         """ drivetemp, motor temp (deg C)"""
-        #INFO - DRVTMP  0x00F8 0x00f9   (1=0.1C)
-        #INFO - MTRTMP  0x00FA 0x00fb
-        return (merge_reg(self.modbus.read_holding_registers(0x00F8, 2, unit=1).registers, bytewid=2, rev=True)*.1,
-                merge_reg(self.modbus.read_holding_registers(0x00FA, 2, unit=1).registers, bytewid=2, rev=True)*.1)
+        # INFO - DRVTMP  0x00F8 0x00f9   (1=0.1C)
+        # INFO - MTRTMP  0x00FA 0x00fb
+        return (merge_reg(self.modbus.read_holding_registers(0x00F8, 2, unit=1).registers, bytewid=2, rev=True) * .1,
+                merge_reg(self.modbus.read_holding_registers(0x00FA, 2, unit=1).registers, bytewid=2, rev=True) * .1)
 
     def move_to(self, position, speed=None, accel=None, decel=None, relative=False, _debreak_sleep=.15):
         """ See pg. 306 & 365 of HM-60262-6E.pdf
@@ -152,9 +158,9 @@ class OrientalMotor(object):
         op_number = 0
         op_type = 3 if relative else 1  # relative to feedback (2 is relative to previous command pos)
         position = int(round(position * MM_TO_PULSE))
-        speed = int(round(min(speed*MM_TO_PULSE if speed is not None else DEFAULT_SPEED, MAX_PULSE_RATE)))
-        accel = int(round(accel*MM_TO_PULSE if accel is not None else DEFAULT_ACCEL))
-        decel = int(round(decel*MM_TO_PULSE if decel is not None else DEFAULT_DECEL))
+        speed = int(round(min(speed * MM_TO_PULSE if speed is not None else DEFAULT_SPEED, MAX_PULSE_RATE)))
+        accel = int(round(accel * MM_TO_PULSE if accel is not None else DEFAULT_ACCEL))
+        decel = int(round(decel * MM_TO_PULSE if decel is not None else DEFAULT_DECEL))
         current = 1000  # 100% in units of .1
         trigger = 1  # move right away
 
@@ -172,8 +178,8 @@ class OrientalMotor(object):
         self.modbus.write_registers(0x058, cmd, unit=1)
 
     def get_position(self):
-        #0x00CC-D detected position, is in steps
-        raw = merge_reg(self.modbus.read_holding_registers(0x00CC, 2, unit=1).registers, bytewid=2, rev=True)
+        # 0x00CC-D detected position, is in steps
+        raw = merge_reg(self.modbus.read_holding_registers(0x00CC, 2, unit=1).registers, rev=True)
         return twos_complement(raw, 32) / MM_TO_PULSE
 
     def stop(self, apply_break=True):
@@ -186,12 +192,12 @@ class OrientalMotor(object):
             self.move_to(0, 0)
 
     def turn_on_break(self):
-        val = merge_reg(self.modbus.read_holding_registers(0x007D, 2, unit=1).registers)  #0x0171 bit0 STOP-COFF
+        val = merge_reg(self.modbus.read_holding_registers(0x007D, 2, unit=1).registers)  # 0x0171 bit0 STOP-COFF
         val |= 0b1
         self.modbus.write_register(0x007D, val, unit=1)
 
     def turn_off_break(self):
-        val = merge_reg(self.modbus.read_holding_registers(0x007D, 2, unit=1).registers)  #0x0171 bit0 STOP-COFF
+        val = merge_reg(self.modbus.read_holding_registers(0x007D, 2, unit=1).registers)  # 0x0171 bit0 STOP-COFF
         val &= 0xfffe
         self.modbus.write_register(0x007D, val, unit=1)
 
@@ -205,65 +211,66 @@ class OrientalMotor(object):
         """
         # code = client.read_holding_registers(0x0080, 2, unit=1).registers
         self.modbus.write_registers(0x01AA, [0, number], unit=1)
-        return self.modbus.read_holding_registers(0x0A00, 26, unit=1).registers
+        regs = self.modbus.read_holding_registers(0x0A00, 26, unit=1).registers
+        regs_iter = iter(regs)
+        vals = [merge_reg((r, next(r)), rev=True) for r in regs_iter]
+        alarm_code = vals[0]  # 66 means sensor error at power on  get if motor disconnected
+        return vals
 
     def reset_alarm(self):
-        #0x0180-1  write 0, then 1
+        # 0x0180-1  write 0, then 1
         self.modbus.write_registers(0x0180, [0, 0], unit=1)
         self.modbus.write_registers(0x0180, [0, 1], unit=1)
-        #0x0184-5 clear alarm records
+        # 0x0184-5 clear alarm records
         self.modbus.write_registers(0x0184, [0, 0], unit=1)
         self.modbus.write_registers(0x0184, [0, 1], unit=1)
-        #0x0188 clear communication error records
+        # 0x0188 clear communication error records
 
     def status(self):
-        outputs = self.get_out() #see bit list excel
+        outputs = self.get_out()  # see bit list excel
         # 0x00AC-D present communcation error
         # 0x00C6-7 command position
         # 0x00CC-D detected position
         # 0x00D6 0x00D7  current/max torque
-        #TODO finish
+        # TODO finish
 
         out_bits = self.get_out(pretty=False)
-        self.get_remoteOut(pretty=False)
+        remote_bits = self.get_remoteOut(pretty=False)
 
-        self.motor_powered = motor_powered  'STOP-COFF_R'
+        self.motor_powered = not is_bit_set(remote_bits, REMOTE_IO_OUT_BITS, 'STOP-COFF_R')
 
-        self.current_ma = current_ma  raw = merge_reg(self.modbus.read_holding_registers(0x00CC, 2, unit=1).registers, bytewid=2, rev=True)
-        self.brake = brake  'MBC'
+        torque_pct = merge_reg(self.modbus.read_holding_registers(0x00D6, 2, unit=1).registers, rev=True)
+
+        self.torque_pct = torque_pct
+        self.brake = is_bit_set(remote_bits, REMOTE_IO_OUT_BITS, 'MBC')
         self.position = self.get_position()
-        self.moving = moving
-        self.ready = ready  'READY'
-        self.inposition = inposition  # 'IN-POS'
-        self.alarm = alarm
-        self.fwlim = fwlim  'FW-LS_R'
-        self.rvlim = rvlim  'RV_LS_R'
-        self.home = home
+        self.moving = is_bit_set(remote_bits, REMOTE_IO_OUT_BITS, 'MOVING')
+        self.ready = is_bit_set(remote_bits, REMOTE_IO_OUT_BITS, 'READY')
+        self.inposition = is_bit_set(remote_bits, REMOTE_IO_OUT_BITS, 'IN-POS')
+        self.alarm = self.read_alarm()[0]
+        self.fwlim = is_bit_set(remote_bits, REMOTE_IO_OUT_BITS, 'FW-LS_R')
+        self.rvlim = is_bit_set(remote_bits, REMOTE_IO_OUT_BITS, 'RV_LS_R')
+
         self.error_string = error_string
-        self.has_fault = has_fault
+        self.has_fault = self.alarm != 0
 
-        REMOTE_IO_OUT_BITS = ('MBC', 'STOP-COFF_R', 'RV_LS_R', '-', 'FW-LS_R', 'READY', 'INFO', 'ALM-A', 'SYS-BSY',
-                              'AREA0', 'AREA1', 'AREA2', 'TIM', 'MOVE', 'IN-POS', 'TLC')
-            if pretty:
-                return tuple([v for i, v in enumerate(OUTPUT_BITS) if x & (1 << i) and v])
+        # REMOTE_IO_OUT_BITS = ('MBC', 'STOP-COFF_R', 'RV_LS_R', '-', 'FW-LS_R', 'READY', 'INFO', 'ALM-A', 'SYS-BSY',
+        #                       'AREA0', 'AREA1', 'AREA2', 'TIM', 'MOVE', 'IN-POS', 'TLC')
 
-        get_out
-        get_directout
         alarm = self.read_alarm()
         pos = self.get_position()
         drive, motor = self.get_temps()
         # self.modbus.read_holding_registers(0x0A00, 26, unit=1).registers
 
         from collections import namedtuple
-        motorIsOn true/false
+        motorIsOn  # true/false
         breakEngaged
         moving
         return x(current, brake, pos, moving, ready, inpos, alarm, fmlim, rvlim, home)
 
 
-
 class OrientalState(object):
-    def __init__(self,  *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
         required attributes:
         'error_string' (if has_fault)
@@ -303,7 +310,6 @@ class OrientalState(object):
         self.error_string = error_string
         self.has_fault = has_fault
 
-
     #
     # @property
     # def calibrated(self):
@@ -317,13 +323,13 @@ class OrientalState(object):
     #     return bin(self.faults.byte) + bin(self.io.errcode)
 
 
+m = OrientalMotor(PORT)
+# if __name__ == '__main__':
+#     logging.basicConfig()
+#     log = logging.getLogger()
+#     log.setLevel(logging.DEBUG)
+#     m = self = OrientalMotor(PORT)
 
-if __name__ == '__main__':
-    logging.basicConfig()
-    log = logging.getLogger()
-    log.setLevel(logging.DEBUG)
-    m = self = OrientalMotor(PORT)
-
--hw lim @-107
-+hw lim @159.5
--104 to 156
+# -hw lim @-107
+# +hw lim @159.5
+# -104 to 156

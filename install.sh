bfa9884fad5682273fefa02f887edc7041045206
Jeb Bailey <baileyji@ucsb.edu>
2020-02-15 16:37:40 -0800

    Install script work

--- install.sh
+++ install.sh
@@ -13,13 +13,13 @@ sudo apt-get auto remove
 sudo apt install python-scipy python-astropy python-matplotlib nut python-flask zsh samba python-pip curl
 sudo pip install --upgrade pip
 sudo apt remove python-serial
-sudo pip install ipython pyserial==2.6 construct==2.0.6 flask_wtf pymodbus==1.5.2
+sudo pip install ipython pyserial==2.6 construct==2.0.6 flask_wtf pymodbus==1.5.2 bitstring==3.1.6
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 
 #Install M2FS-Control
 cd /
-sudo git clone https://github.com/YSAS/M2FS-Control.git /M2FS-Control --recurse-submodules --branch develop
+sudo git clone https://github.com/baileyji/M2FS-Control.git /M2FS-Control --recurse-submodules --branch develop
 sudo chown -R pi /M2FS-Control
 cd /M2FS-Control
 
@@ -33,6 +33,7 @@ sudo cp -v ./etc/hostname /etc/
 sudo cp -v ./etc/hosts /etc/
 sudo cp -v ./etc/dhcpcd.conf /etc/
 sudo cp -v ./etc/udev/rules.d/m2fs.rules /etc/udev/rules.d/
+sudo cp -v ./etc/udev/rules.d/ifum.rules /etc/udev/rules.d/
 sudo cp -v ./etc/systemd/system/* /etc/systemd/system/ 
 sudo cp -v ./etc/systemd/timesyncd.conf /etc/systemd/
 sudo cp -v ./etc/avahi/services/smb.service /etc/avahi/services/
@@ -41,6 +42,9 @@ sudo cp -v ./etc/samba/smb.conf /etc/samba/
 sudo cp -v ./nut-driver.service /lib/systemd/system/nut-driver.service
 #cp -v ./vimrc /home/root/.vimrc
 #cp ./etc/ntp.conf /etc/
+
+sudo udevadm control --reload-rules
+sudo udevadm trigger
 
 #Ensure systemd loads the new units
 sudo systemctl daemon-reload

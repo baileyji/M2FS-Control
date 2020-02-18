7ff313c1a46d4ef077cda4d05af95a67bb85ea06
Jeb Bailey <baileyji@ucsb.edu>
2020-02-17 21:10:47 -0800

    install work

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from distutils.core import setup
-
-setup(name='m2fscontrol',
+import setuptools
+setuptools.setup(name='m2fscontrol',
       version='1.0',
       description='M2FS Control Python Libraries',
       author='Jeb Bailey',
       author_email='baileyji@umich.edu',
-      url='https://www.jebbailey.com',
-      packages=['m2fscontrol', 'distutils.command'],
+      url="https://github.com/baileyji/M2FS-Control",
+      packages=setuptools.find_packages(),
+      classifiers=("Programming Language :: Python :: 2",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: POSIX",
+                   "Intended Audience :: Science/Research"),
+      install_requires=['numpy>=1.8.0', 'jbastro', 'hole-mapper']
 )

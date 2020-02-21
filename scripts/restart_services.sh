d1a5efe240bf6d144262a11983219da9d4312305
Jeb Bailey <baileyji@ucsb.edu>
2020-02-21 09:46:14 -0800

    update restart script

--- scripts/restart_services.sh
+++ scripts/restart_services.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-
 #  restart_services.sh
 #  m2fs
 #
 #  Created by Jeb Bailey on 7/25/12.
-#  Copyright (c) 2012 SpaceColonyOne, Inc. All rights reserved.
 systemctl --system daemon-reload
 systemctl restart m2fs_shoeR.service
 systemctl restart m2fs_shoeB.service
@@ -15,3 +13,14 @@ systemctl restart m2fs_shackhartman.service
 systemctl restart m2fs_plugController.service
 systemctl restart m2fs_guider.service
 systemctl restart m2fs_director.service
+systemctl restart m2fs_mcalled.service
+systemctl restart m2fs_shutdown_button.service
+systemctl restart m2fs_datalogger.service
+
+systemctl restart ifum_selector.service
+systemctl restart ifum_occulterH.service
+systemctl restart ifum_occulterH.service
+systemctl restart ifum_occulterH.service
+systemctl restart ifum_shield.service
+systemctl restart ifum_shoe.service
+

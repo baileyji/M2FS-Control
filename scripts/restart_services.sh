969dbe8c67532245b9819277695f077474181fc1
Jeb Bailey <baileyji@ucsb.edu>
2022-04-18 22:59:56 -0400

    must be sudo.

--- scripts/restart_services.sh
+++ scripts/restart_services.sh
@@ -3,25 +3,27 @@
 #  m2fs
 #
 #  Created by Jeb Bailey on 7/25/12.
-systemctl --system daemon-reload
-systemctl restart m2fs_shoeR.service
-systemctl restart m2fs_shoeB.service
-systemctl restart m2fs_galilR.service
-systemctl restart m2fs_galilB.service
-systemctl restart m2fs_slitController.service
-systemctl restart m2fs_shackhartman.service
-systemctl restart m2fs_plugController.service
-systemctl restart m2fs_guider.service
-systemctl restart m2fs_director.service
-systemctl restart m2fs_mcalled.service
-systemctl restart m2fs_shutdown_button.service
-systemctl restart m2fs_datalogger.service
+sudo systemctl --system daemon-reload
 
-systemctl restart ifum_selector.service
-systemctl restart ifum_occulterH.service
-systemctl restart ifum_occulterH.service
-systemctl restart ifum_occulterH.service
-systemctl restart ifum_shield.service
-systemctl restart ifum_shoe.service
+sudo systemctl restart m2fs_galilR.service
+sudo systemctl restart m2fs_galilB.service
+sudo systemctl restart m2fs_director.service
+sudo systemctl restart m2fs_shutdown_button.service
+sudo systemctl restart m2fs_datalogger.service
 
-systemctl restart redis-server.service
+sudo systemctl restart redis-server.service
+
+sudo systemctl restart m2fs_shoeR.service
+sudo systemctl restart m2fs_shoeB.service
+sudo systemctl restart m2fs_slitController.service
+sudo systemctl restart m2fs_shackhartman.service
+sudo systemctl restart m2fs_plugController.service
+sudo systemctl restart m2fs_guider.service
+sudo systemctl restart m2fs_mcalled.service
+
+sudo systemctl restart ifum_selector.service
+sudo systemctl restart ifum_occulterH.service
+sudo systemctl restart ifum_occulterH.service
+sudo systemctl restart ifum_occulterH.service
+sudo systemctl restart ifum_shield.service
+sudo systemctl restart ifum_shoe.service
